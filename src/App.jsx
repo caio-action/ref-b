cb79bb71f4f6f2324dbd115d2e84813221e2d46a
caiotebayo <caiofullstack@gmail.com>
2022-06-05 19:46:39 -0300

    ref-b

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import React from 'react'
+// import CV from '../../Assets/Profile.pdf'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <a href={'#'} download className='btn'>Baixar a Bibliografia</a>
+        <div className='container'>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime animi beatae ipsum quidem repudiandae reiciendis ipsam amet nisi voluptates ratione laborum provident consequatur architecto, tenetur corporis libero modi repellat temporibus!
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
