a8093ad0db7fcd6e48a82168f1465fc038979966
caiotebayo <caiofullstack@gmail.com>
2022-06-07 03:42:34 -0300

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,103 @@
 import './App.css';
 import React from 'react'
-// import CV from '../../Assets/Profile.pdf'
+import CV from '../../ref-b/src/Assets/Referência Bibliográfica.pdf'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      <a href={'#'} download className='btn'>Baixar a Bibliografia</a>
-        <div className='container'>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime animi beatae ipsum quidem repudiandae reiciendis ipsam amet nisi voluptates ratione laborum provident consequatur architecto, tenetur corporis libero modi repellat temporibus!
-        </div>
+      <header className='header' >
+        <a href={CV} download className='btn'>Baixar a Bibliografia</a>
       </header>
+     
+        <article className='container'>
+          <h1>Referência Bibliográfica:</h1>
+          <p>-Relatório e entrevista com jornalista sírio,sobre tropas sunitas vindo para a Síria pela Jordânia e Turquia,aqui:
+          </p><a href='http://www.boilingfrogspost.com/2011/12/11/bfp-exclusive-developing-story-hundreds-of-us-nato-soldiers-arrive-begin-operations-on-the-jordan-syria-border/' target={'_blank'}> <p>http://www.boilingfrogspost.com/2011/12/11/bfp-exclusive-developing-story-hundreds-of-us-nato-soldiers-arrive-begin-operations-on-the-jordan-syria-border/</p></a><p>e aqui: <a href='https://www.youtube.com/watch?v=-v1h1bUfCVca' target={'_blank'}> https://www.youtube.com/watch?v=-v1h1bUfCVca</a></p>
+          
+          <div class="linha-vertical"></div>
+
+
+          <p></p><a href=""><p></p></a>
+
+
+          <p>sobre os motivos externos da guerra :</p>
+          
+          <a href="https://revistaforum.com.br/blogs/blog-da-maria-fr/2011/12/14/pepe-escobar-sonho-da-otan-guerra-civil-na-siria-45609.html">
+            <p>https://revistaforum.com.br/blogs/blog-da-maria-fr/2011/12/14/pepe-escobar-sonho-da-otan-guerra-civil-na-siria-45609.html</p></a>
+          <a href="https://revistaforum.com.br/blogs/blog-da-maria-fr/2011/12/14/pepe-escobar-sonho-da-otan-guerra-civil-na-siria-45609.html
+          https://redecastorphoto.blogspot.com/2011/12/pepe-escobar-guerra-de-sombras-na-siria.html"><p>https://revistaforum.com.br/blogs/blog-da-maria-fr/2011/12/14/pepe-escobar-sonho-da-otan-guerra-civil-na-siria-45609.html
+              https://redecastorphoto.blogspot.com/2011/12/pepe-escobar-guerra-de-sombras-na-siria.html</p></a>
+          <a href="http://www.orientemidia.org/pepe-escobar-na-estrada-de-damasco-como-foi-vencida-a-guerra-na-siria/"><p>http://www.orientemidia.org/pepe-escobar-na-estrada-de-damasco-como-foi-vencida-a-guerra-na-siria/</p></a>
+          <a href="http://www.orientemidia.org/pepe-escobar-a-guerra-do-oleogasodutostao-na-siria-guerra-de-negocios-nao-de-municao/"><p>http://www.orientemidia.org/pepe-escobar-a-guerra-do-oleogasodutostao-na-siria-guerra-de-negocios-nao-de-municao /</p></a>
+          <div class="linha-vertical"></div>
+          <p>"O Cinturão e as Rotas da Seda" :</p><a href="https://dialogosdosul.operamundi.uol.com.br/china/52482/china-e-o-projeto-estrategico-mais-importante-da-historia"><p>https://dialogosdosul.operamundi.uol.com.br/china/52482/china-e-o-projeto-estrategico-mais-importante-da-historia</p></a>
+          <div class="linha-vertical"></div>
+          <p> Corredor de Transportes Internacional Norte-Sul("CoTins") e como será conectada a eurásia:</p><a href="http://www.orientemidia.org/pepe-escobar-como-a-eurasia-sera-interconectada/"><p>http://www.orientemidia.org/pepe-escobar-como-a-eurasia-sera-interconectada/</p></a>
+          <div class="linha-vertical"></div>
+          <p>"Wahhabismo" :</p><a href="https://www.bbc.com/portuguese/noticias/2016/01/151222_wahabismo_origens_fn"><p className='links'>https://www.bbc.com/portuguese/noticias/2016/01/151222_wahabismo_origens_fn</p></a>
+          <div class="linha-vertical"></div>
+          <p> Explicação a respeito do Curdistão e interesses curdos na Síria :</p><a href="https://asiatimes.com/2019/10/kurds-face-stark-options-after-us-pullback/"><p>https://asiatimes.com/2019/10/kurds-face-stark-options-after-us-pullback/</p></a>
+
+          <a href="https://www.brasil247.com/blog/curdos-enfrentam-alternativas-sombrias-depois-da-retirada-das-tropas-dos-estados-unidos"><p>https://www.brasil247.com/blog/curdos-enfrentam-alternativas-sombrias-depois-da-retirada-das-tropas-dos-estados-unidos</p></a>
+          <div class="linha-vertical"></div>
+          <p>Arábia Saudita x Irã :</p><a href="https://www.cartacapital.com.br/carta-explica/por-que-arabia-saudita-rompeu-relacoes-com-o-ira/"><p>https://www.cartacapital.com.br/carta-explica/por-que-arabia-saudita-rompeu-relacoes-com-o-ira/</p></a>
+          <div class="linha-vertical"></div>
+          <p>Importancia das "Novas Rotas da Seda" e os interesses de Turquia e Catar na guerra :</p><a href="http://www.orientemidia.org/pepe-escobara-nova-rota-da-seda-poderia-se-tornar-o-nucleo-de-uma-ordem-mundial-realmente-nova-mas-nenhuma-de-acordo-com-o-gosto-dos-estados-unidos/"><p>http://www.orientemidia.org/pepe-escobara-nova-rota-da-seda-poderia-se-tornar-o-nucleo-de-uma-ordem-mundial-realmente-nova-mas-nenhuma-de-acordo-com-o-gosto-dos-estados-unidos/</p></a>
+          <div class="linha-vertical"></div>
+          <p>sobre Guerra Híbrida/Guerra da informação :</p><a href="https://www.brasil247.com/mundo/wikileaks-eua-querem-derrubar-assad-desde-2006"><p>https://www.brasil247.com/mundo/wikileaks-eua-querem-derrubar-assad-desde-2006</p></a>
+       <a href="https://www.brasil247.com/mundo/wikileaks-divulgara-2-4-milhoes-de-emails-sobre-a-siria"><p>https://www.brasil247.com/mundo/wikileaks-divulgara-2-4-milhoes-de-emails-sobre-a-siria</p></a>
+        <a href="https://revistaforum.com.br/global/2013/9/1/wikileaks-eua-tm-planos-para-derrubar-assad-desde-2006-7394.html"><p>https://revistaforum.com.br/global/2013/9/1/wikileaks-eua-tm-planos-para-derrubar-assad-desde-2006-7394.html</p></a>
+          
+          <p>Wikileaks :</p><a href="https://www.wikileaks.org/plusd/cables/06DAMASCUS5399_a.html#efmBA8BJR"><p className='links'>https://www.wikileaks.org/plusd/cables/06DAMASCUS5399_a.html#efmBA8BJR</p></a>
+          <p>Concessão de Assad e uma semana depois protestos violêntos em Damasco,Deraa,Homs e Hirak:</p><a href="https://www.bbc.com/portuguese/noticias/2011/04/110422_video_siria_rp"><p className='links'>https://www.bbc.com/portuguese/noticias/2011/04/110422_video_siria_rp</p></a>
+          <div class="linha-vertical"></div>
+
+          <p>Julho de 2011 :</p><a href="https://www.bbc.com/portuguese/noticias/2011/08/110812_siria_rc"><p>https://www.bbc.com/portuguese/noticias/2011/08/110812_siria_rc</p></a>
+          <div class="linha-vertical"></div>
+
+          <p>Julho de 2012 -corroboram as denuncias do governo  de que instrutores franceses e ingleses estivessem desde 2009 ,em Hatay,na Turquia treinando jihaadistas wahhabitas e mercenários estrangeiros ,com a intensão de atingir a síria,e principalmente Aleppo :</p><a href="http://www.boilingfrogspost.com/2011/12/11/bfp-exclusive-developing-story-hundreds-of-us-nato-soldiers-arrive-begin-operations-on-the-jordan-syria-border/"><p>http://www.boilingfrogspost.com/2011/12/11/bfp-exclusive-developing-story-hundreds-of-us-nato-soldiers-arrive-begin-operations-on-the-jordan-syria-border/</p></a>
+          <p>Ataque dos Foguetes e ambas as versões :</p><a href="https://www.bbc.com/portuguese/noticias/2013/08/130822_qanda_siria_fl"><p className='links'>https://www.bbc.com/portuguese/noticias/2013/08/130822_qanda_siria_fl</p></a>
+          <a href="https://exame.com/mundo/suposto-ataque-quimico-mata-centenas-na-siria-veja-fotos/"><p>https://exame.com/mundo/suposto-ataque-quimico-mata-centenas-na-siria-veja-fotos/</p></a>
+          <div class="linha-vertical"></div>
+
+          <p>Novembro de 2013 :</p><a href="https://www.terra.com.br/noticias/mundo/rebeldes-dizem-ter-tomado-maior-campo-de-petroleo-da-siria,e0cdf41b62082410VgnCLD2000000dc6eb0aRCRD.html"><p>https://www.terra.com.br/noticias/mundo/rebeldes-dizem-ter-tomado-maior-campo-de-petroleo-da-siria,e0cdf41b62082410VgnCLD2000000dc6eb0aRCRD.html</p></a>
+          <div class="linha-vertical"></div>
+
+          <p>Junho de 2014 :</p><a href="https://www.bbc.com/portuguese/noticias/2014/06/140630_entenda_califado_an_hb">https://www.bbc.com/portuguese/noticias/2014/06/140630_entenda_califado_an_hb</a>
+          <p>Armamento do ISIs pelos EUA :</p><a href="https://outraspalavras.net/outrasmidias/wikileaks-eua-armaram-terror-do-estado-islamico/"><p>https://outraspalavras.net/outrasmidias/wikileaks-eua-armaram-terror-do-estado-islamico/</p></a>
+          <a href=" https://search.wikileaks.org/?q=%22Islamic+State+of+Iraq%22&sort=2"><p> https://search.wikileaks.org/?q=%22Islamic+State+of+Iraq%22&sort=2</p></a>
+          <div class="linha-vertical"></div>
+
+          <p>Abril de 2014 :</p><a href="https://www.rfi.fr/br/geral/20140618-opaq-denuncia-uso-sistematico-de-armas-quimicas-na-siria"><p>https://www.rfi.fr/br/geral/20140618-opaq-denuncia-uso-sistematico-de-armas-quimicas-na-siria</p></a>    
+          <div class="linha-vertical"></div>
+
+          <p>Setembro de 2014 :</p><a href=" https://www.terra.com.br/noticias/mundo/oriente-medio/bombardeios-de-coalizao-internacional-na-siria-ja-deixaram-4-mil-mortos,4b9286edbe7ee7c2d4c00378fb7b48efh0qz085t.html#:~:text=A%20coaliz%C3%A3o%20internacional%20come%C3%A7ou%20a%20bombardear%20o%20EI,reivindicados%20pelos%20extremistas%20e%20que%20deixaram%20130%20mortos"><p>https://www.terra.com.br/noticias/mundo/oriente-medio/bombardeios-de-coalizao-internacional-na-siria-ja-deixaram-4-mil-mortos</p></a>        
+          <div class="linha-vertical"></div>
+
+          <p> Março de 2016- Exército de Al Assad retoma as ruínas históricas de Palmira :</p><a href="https://brasil.elpais.com/brasil/2016/03/26/internacional/1459027459_392910.html"><p>https://brasil.elpais.com/brasil/2016/03/26/internacional/1459027459_392910.html</p></a>        
+          <a href="https://br.sputniknews.com/20180319/siria-ghouta-oriental-bashar-assad-soldados-fotos-video-10772716.html"><p>https://br.sputniknews.com/20180319/siria-ghouta-oriental-bashar-assad-soldados-fotos-video-10772716.html</p></a>        
+          <p> Defesa Russa a respeito das fake news ocidentais e do papel da Rússia na reconciliação do conflito nesse ano  :</p><a href="https://br.sputniknews.com/organization_Centro_de_Reconciliacao_Siria_do_Ministerio_da_Defesa_da_Russia/"></a>        
+          <div class="linha-vertical"></div>
+
+          <p>Setembro de 2016:</p><a href="https://www.brasil247.com/mundo/presidente-sirio-comemora-reconquista-de-aleppo"><p>https://www.brasil247.com/mundo/presidente-sirio-comemora-reconquista-de-aleppo</p></a>        
+          <p>Janeiro de 2017:</p><a href="http://www.orientemidia.org/pepe-escobar-o-cazaquistao-talvez-possua-o-segredo-para-a-grande-eurasia/"><p>http://www.orientemidia.org/pepe-escobar-o-cazaquistao-talvez-possua-o-segredo-para-a-grande-eurasia/</p></a> 
+          <div class="linha-vertical"></div>       
+          <p>Janeiro de 2018</p><a href="http://www.orientemidia.org/consenso-entre-sirios-em-sochi/"><p>http://www.orientemidia.org/consenso-entre-sirios-em-sochi/</p></a> 
+          <div class="linha-vertical"></div>
+          <p>Fevereiro de 2018</p><a href="https://br.sputniknews.com/20180319/siria-ghouta-oriental-bashar-assad-soldados-fotos-video-10772716.html"><p>https://br.sputniknews.com/20180319/siria-ghouta-oriental-bashar-assad-soldados-fotos-video-10772716.html</p></a> 
+          <div class="linha-vertical"></div>
+          <p>Abril de 2018 :</p><a href="https://mundo.sputniknews.com/20170512/eeuu-ataque-quimico-siiria-investigacion-1069118983.html"><p>https://mundo.sputniknews.com/20170512/eeuu-ataque-quimico-siiria-investigacion-1069118983.html</p></a> 
+          <a href="https://globoplay.globo.com/v/6696440/"><p>https://globoplay.globo.com/v/6696440/</p></a> 
+          <a href="https://br.sputniknews.com/20200121/orgao-de-controle-de-armas-quimicas-manipulou-informacoes-sobre-ataque-na-siria-revela-delator-15030693.html"><p>https://br.sputniknews.com/20200121/orgao-de-controle-de-armas-quimicas-manipulou-informacoes-sobre-ataque-na-siria-revela-delator-15030693.html</p></a> 
+          <a href="https://br.sputniknews.com/20170406/armas-quimicas-onu-siria-8085557.html"><p>https://br.sputniknews.com/20170406/armas-quimicas-onu-siria-8085557.html</p></a> 
+
+          <div class="linha-vertical"></div>
+          <p>Outubro de 2019 :</p><a href="https://www.brasil247.com/blog/a-reconstrucao-da-siria-sem-o-petroleo-sirio"><p>https://www.brasil247.com/blog/a-reconstrucao-da-siria-sem-o-petroleo-sirio</p></a> 
+          <div class="linha-vertical"></div>
+          <p>Julho de 2021 :</p><a href="https://pcdob.org.br/noticias/base-dos-eua-no-campo-petrolifero-de-omar-na-siria-e-bombardeada/"><p>https://pcdob.org.br/noticias/base-dos-eua-no-campo-petrolifero-de-omar-na-siria-e-bombardeada/</p></a> 
+
+          
+        </article>
     </div>
   );
 }
